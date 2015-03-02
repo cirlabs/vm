0ccf6f68f57195b022212b2904954ee8f0f45311
Aaron Williams <aaron.colby.williams@gmail.com>
2015-03-02 15:27:46 -0800

    nohup swah

--- sync.sh
+++ sync.sh
@@ -5,13 +5,18 @@
 VOLUME_DIR='media'
 ASSET_PATH=$HOME/Desktop/TechRaking
 
-for mtabline in `cat /etc/mtab | grep $VOLUME_DIR`; do
-    device_path=`echo $mtabline | grep media`
+sync_all () {
+    for mtabline in `cat /etc/mtab | grep $VOLUME_DIR`; do
+        device_path=`echo $mtabline | grep media`
 
-    if [ $? == 0 ]; then
-        echo "Syncing ..."
-        set -x
-        rsync -aviuP $ASSET_PATH $device_path
-        set +x
-    fi
-done
+        if [ $? == 0 ]; then
+            echo "Syncing ..."
+            set -x
+            nohup rsync -aviuP --modify-window=1 --update $ASSET_PATH $device_path &
+            set +x
+        fi
+    done
+}
+
+# Run
+sync_all
