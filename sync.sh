a21926cb728b422dfd6ecc01a48b5b63c7adafec
Aaron Williams <aaron.colby.williams@gmail.com>
2015-03-02 14:11:06 -0800

    rsync to sync drive

--- sync.sh
+++ sync.sh
@@ -3,14 +3,15 @@
 # adapted from http://unix.stackexchange.com/a/119260/105076
 
 VOLUME_DIR='media'
-USB_PATHS=()
+ASSET_PATH=$HOME/Desktop/TechRaking
 
 for mtabline in `cat /etc/mtab | grep $VOLUME_DIR`; do
     device_path=`echo $mtabline | grep media`
 
     if [ $? == 0 ]; then
-        touch `$device_path/file.txt`
+        echo "Syncing ..."
+        set -x
+        rsync -aviuP $ASSET_PATH $device_path
+        set +x
     fi
 done
-
-echo $USB_PATHS
