ec8791e053024c1f1e1c8dfc769f5b9b5e801d0a
Aaron Williams <aaron.colby.williams@gmail.com>
2015-03-02 15:46:46 -0800

    store in function

--- sync.sh
+++ sync.sh
@@ -5,13 +5,17 @@
 VOLUME_DIR='media'
 ASSET_PATH=$HOME/Desktop/TechRaking
 
-for mtabline in `cat /etc/mtab | grep $VOLUME_DIR`; do
-    device_path=`echo $mtabline | grep media`
+sync_all() {
+    for mtabline in `cat /etc/mtab | grep $VOLUME_DIR`; do
+        device_path=`echo $mtabline | grep media`
 
-    if [ $? == 0 ]; then
-        echo "Syncing ..."
-        set -x
-        rsync -aviuP $ASSET_PATH $device_path
-        set +x
-    fi
-done
+        if [ $? == 0 ]; then
+            echo "Syncing ..."
+            set -x
+            rsync -aviuP $ASSET_PATH $device_path
+            set +x
+        fi
+    done
+}
+
+sync_all
