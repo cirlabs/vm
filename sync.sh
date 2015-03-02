dc6fa2732b0fefb6c34c49c4bfb98acc4fa5bd9e
Aaron Williams <aaron.colby.williams@gmail.com>
2015-03-02 15:45:41 -0800

    Revert "nohup swah"
    This reverts commit 0ccf6f68f57195b022212b2904954ee8f0f45311.

--- sync.sh
+++ sync.sh
@@ -5,18 +5,13 @@
 VOLUME_DIR='media'
 ASSET_PATH=$HOME/Desktop/TechRaking
 
-sync_all () {
-    for mtabline in `cat /etc/mtab | grep $VOLUME_DIR`; do
-        device_path=`echo $mtabline | grep media`
+for mtabline in `cat /etc/mtab | grep $VOLUME_DIR`; do
+    device_path=`echo $mtabline | grep media`
 
-        if [ $? == 0 ]; then
-            echo "Syncing ..."
-            set -x
-            nohup rsync -aviuP --modify-window=1 --update $ASSET_PATH $device_path &
-            set +x
-        fi
-    done
-}
-
-# Run
-sync_all
+    if [ $? == 0 ]; then
+        echo "Syncing ..."
+        set -x
+        rsync -aviuP $ASSET_PATH $device_path
+        set +x
+    fi
+done
