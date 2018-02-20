3972470bd732e85d7f13856204dc3aa24243b3fa
esagara <esagara@gmail.com>
2018-02-20 13:38:47 -0800

    added R, pyenv, sqlite3, etc

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,16 +10,29 @@ print_header() {
 }
 
 print_header "updating ubuntu"
-#Add repositories for QGIS, Atom, Sublime Text 3 and Tor Browser bundle
+#Add repositories for QGIS, Sublime Text 3, R and Tor Browser bundle
 echo "deb http://qgis.org/debian xenial main" | sudo tee -a /etc/apt/sources.list
 echo "deb-src http://qgis.org/debian xenial main" | sudo tee -a /etc/apt/sources.list
-#add gpg key for qgis download and install
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 073D307A618E5811
 
-sudo add-apt-repository -y ppa:webupd8team/atom
-# sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+#add gpg key for qgis download and install
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key CAEB3DC3BDF7FB45
+
+# add gpg key for Sublime Text 3
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+
+# allow apt to work with https sources
+sudo apt-get install apt-transport-https
+
+# select the stable version of Sublime Text 3
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
 sudo add-apt-repository -y ppa:webupd8team/tor-browser
 
+# point apt to the ubuntu repository for R
+sudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" | sudo tee -a /etc/apt/sources.list
+
+# add gpg key for R
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key E084DAB9
 
 
 #Update apt-get and upgrade packages
@@ -27,41 +40,60 @@ sudo apt-get -qq update
 sudo apt-get -qq upgrade
 
 # development
-print_header "installing development tools"
+print_header "Installing development tools"
 sudo apt-get -qq install build-essential fortune cowsay
 
 # Git
-print_header "installing git"
+print_header "Installing git"
 sudo apt-get -qq install git-core
 
-# zsh
-# print_header "installing zsh and oh-my-zsh"
-# wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+# Sublime Text 3
+print_header "Installing Sublime Text 3"
+sudo apt-get install sublime-text
+
+# pyenv
+print_header "Installing pyenv"
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+pyenv update
+
+pyenv install 3.6.4
+pyenv install 2.7.14
+
+pyenv global 3.6.4
+
+mkdir code
+mkdir code/python2 && cd $_
+pyenv virtualenv 2.7.14 python-2
+cd ..
+mkdir python3 && cd $_
+pyenv virtualenv 3.6.4 python-3
+cd ~
+
+# R and R-Studio
+print_header "Installing R"
+sudo apt-get install r-base r-base-dev
 
 # python scientific stack
-print_header "installing python scientific stack"
-sudo apt-get -qq install python-numpy python-scipy python-matplotlib ipython ipython-doc ipython-notebook ipython-qtconsole python-virtualenv python-dev python-pip python-sip pyqt4-dev-tools
+print_header "Installing python scientific stack"
+sudo apt-get -qq install python-numpy python-scipy python-matplotlib python-dev python-pip python-sip pyqt4-dev-tools
 
 # various Python libraries we like
 print_header "pip installing favored Python libraries"
+sudo pip install --quiet jupyter
 sudo pip install --quiet beautifulsoup4
 sudo pip install --quiet requests
-sudo pip install --quiet django
-sudo pip install --quiet virtualenvwrapper
+sudo pip install --quiet django==1.11.10
 sudo pip install --quiet pandas
 sudo pip install --quiet csvkit
 sudo pip install --quiet miditime
 sudo pip install --quiet flask
 sudo pip install --quiet agate
+sudo pip install --quiet psycopg2
+sudo pip install --quiet altair
 
-# make sure virtalenvwrapper is loaded and works
-print_header "  setting up virtualenvwrapper"
-mkdir .envs
-echo "export WORKON_HOME=$HOME/.envs" >> ~/.bashrc
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+# enable ipyvega for altair to work
+jupyter nbextension enable --sys-prefix --py vega
 
-echo "export WORKON_HOME=$HOME/.envs" >> ~/.zshrc
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.zshrc
 
 # postgres
 print_header "installing latest PostgreSQL and PostGIS"
@@ -73,6 +105,10 @@ echo "  IMPORTANT: Remember to create a Postgres superuser for your user!"
 # MySQL
 print_header "Installing MySQL"
 sudo apt-get -qq install mysql-server mysql-client libmysqlclient-dev
+
+# Sqlite3
+print_header "Installing Sqlite3"
+sudo apt-get install sqlite3 libsqlite3-dev
 
 #qgis
 print_header "installing QGIS"
@@ -91,13 +127,6 @@ sudo ln -s /usr/bin/nodejs /usr/bin/node
 #npm
 sudo apt-get install -y npm
 
-#Atom text editor
-print_header "installing Atom text editor"
-sudo apt-get install -y atom
-
-#Sublime text 3
-# print_header "installing Sublime Text 3"
-# sudo apt-get install sublime-text-installer
 
 #Tor Browser
 sudo apt-get install tor-browser
@@ -122,15 +151,12 @@ sudo apt-get install -qq autoconf bison libssl-dev libyaml-dev libreadline6-dev 
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.zshrc
 
 #install ruby-build
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 source ~/.bashrc
-# source ~/.zshrc
 
 #install latest stable version of Ruby
 rbenv install 2.2.0
